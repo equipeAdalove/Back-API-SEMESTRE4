ca5be3d67ccfc67d4cee3610ee50093963539c69
Madhs31 <fernandinha.mf30@gmail.com>
2025-10-22 11:21:56 -0300

    🧱 chore: registrar rotas de usuário e autenticação no app FastAPI

--- app/main.py
+++ app/main.py
@@ -1,27 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import pdf_routes, test_routes
+from routes import pdf_routes, test_routes, user_routes, auth_routes
 from contextlib import asynccontextmanager
 from models import models
 from database.database import engine
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    yield
 
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    models.Base.metadata.create_all(bind=engine)
+    yield
+    
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(pdf_routes.router, prefix="/api", tags=["PDF Processing"])
+app.include_router(user_routes.router, prefix="/api", tags=["Usuários"])
+app.include_router(auth_routes.router, prefix="/api", tags=["Autenticação"])
+app.include_router(pdf_routes.router, prefix="/api")
 app.include_router(test_routes.router, prefix="/api", tags=["TESTE"])
-
-@app.get("/")
-async def read_root():
-    return {"message": "API AdaTech está no ar!"}
