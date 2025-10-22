05461070cc9097f6c0a3327f6c17333e4bb78304
Madhs31 <fernandinha.mf30@gmail.com>
2025-10-22 11:23:30 -0300

    🛠️ chore: Adicionando variáveis de configuração NCM_CSV_PATH e TOP_K

---
+++ config.py
@@ -0,0 +1,9 @@
+# C:\Users\ferna\Desktop\API\Back-API-SEMESTRE4\config.py
+
+# Caminho para o seu arquivo CSV
+# Substitua pelo caminho real do arquivo NCM se ele existir
+NCM_CSV_PATH = "data/ncm_list.csv" 
+
+# O valor Top-K usado em alguma função de ranking ou busca
+# Mantenha como um número inteiro
+TOP_K = 5
